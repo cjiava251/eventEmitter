ece35f01a8f3b4bc4e17ad90a3cc6b1e8e34974f
cjiava251 <zlivkosyava@gmail.com>
2019-02-26 18:30:40 +0300

    maybe it was need doing

--- eventemitter.js
+++ eventemitter.js
@@ -1,1 +1,15 @@
-const fs=require('fs');
+const EventEmitter=require('events');
+const myEmitter =new EventEmitter();
+myEmitter.on('hello', function(a) {
+    console.log(a);
+});
+myEmitter.on('how are you?', function(a) {
+    console.log(a);
+});
+myEmitter.on('what are you doing?', function(a) {
+    console.log(a);
+});
+myEmitter.emit('hello','hello');
+myEmitter.emit('how are you?','i am fine');
+myEmitter.emit('what are you doing?', 'i am walking');
+
