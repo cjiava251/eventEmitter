d3c89ecc9ede1810b4950b183c0d4fa37b4b5a05
cjiava251 <zlivkosyava@gmail.com>
2019-02-27 12:35:03 +0300

    version with russian words

--- eventemitter.js
+++ eventemitter.js
@@ -9,7 +9,20 @@ myEmitter.on('how are you?', function(a) {
 myEmitter.on('what are you doing?', function(a) {
     console.log(a);
 });
+
 myEmitter.emit('hello','hello');
 myEmitter.emit('how are you?','i am fine');
 myEmitter.emit('what are you doing?', 'i am walking');
 
+myEmitter.on('привет', function() {
+    console.log('здравствуй');
+});
+myEmitter.on('как дела?', function() {
+    console.log('все отлично');
+});
+myEmitter.on('что делаешь?', function() {
+    console.log('гуляю');
+});
+myEmitter.emit('привет');
+myEmitter.emit('как дела?');
+myEmitter.emit('что делаешь?');
