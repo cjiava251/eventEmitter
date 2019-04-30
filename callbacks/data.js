5f0bf4a1c3b49258a68993b42ab7d43103d3e943
cjiava251 <zlivkosyava@gmail.com>
2019-04-30 14:01:50 +0300

    final version with callbacks

--- callbacks/data.js
+++ callbacks/data.js
@@ -1,7 +1,27 @@
-const fs=require('fs');
-var data=fs.readFileSync('data.json','utf8');
-var template=fs.readFileSync('template.html','utf8');
-var lib=require('mustache')
-var output=lib.render(template,data);
+const fs = require('fs');
+const libMustache = require('mustache');
 
+let dataJson; let template; let view;
+fs.readFile('data.json', 'utf-8', (err, data) => {
+	if (err) throw err;
+	else dataJson = data;
+	view = JSON.parse(dataJson);
+});
+fs.readFile('template.html', 'utf-8', (err, data) => {
+	if (err) throw err;
+	else template = data;
+});
 
+function waitForReadFile() {
+	setTimeout(() => {
+		if ((!view) || (!template)) waitForReadFile();
+		else {
+			const output = libMustache.render(template, view);
+			fs.writeFile('build.html', output, (err) => {
+				if (err) throw err;
+				else console.log('Success!');
+			});
+		}
+	}, 100);
+}
+waitForReadFile();
