335cde139a14c2ce9a65bbdcd1c05d743c05f2c4
cjiava251 <zlivkosyava@gmail.com>
2019-05-15 14:27:35 +0300

    worked version before checking

--- streams/data.js
+++ streams/data.js
@@ -1,18 +1,31 @@
-const fs=require('fs');
-function tail(readableFile,stringCount,writableFile) {
-    let readableStream=fs.createReadStream(readableFile,{encoding: 'utf-8',start: 0});
-    let writableStream=fs.createWriteStream(writableFile);
-    var data,stringOfData,splicedData;
-    readableStream.on('data',(chunk) => {
-        data=chunk.split('\n');
-        if (data.length>=stringCount)
-        {
-            splicedData=data.splice(data.length-stringCount,stringCount);
-            stringOfData=splicedData.join('\n');
-            writableStream.write(stringOfData);
-            readableStream.pipe(writableStream);
-        }
-        else throw new Error('Too many strings');
-    });
+const fs = require('fs');
+/*
+let writableStream=fs.createWriteStream('bigfile.txt');
+for (let i=0;i<=100;i++)
+    writableStream.write('qwertyuiopfghjgderhhgdfghdfty767567856fdbhfghdfghf3224dsfhbddfhjgdr\n');
+    */
+
+function tail(readableFile, writableFile, stringCount) {
+  const readableStream = fs.createReadStream(readableFile, { encoding: 'utf-8', start: 0 });
+  const writableStream = fs.createWriteStream(writableFile);
+  const tailStream = fs.createWriteStream('laststrings.txt');
+  readableStream.pipe(writableStream);
+  if (stringCount === undefined)
+    stringCount = 10;
+  let data = '';
+  readableStream.on('data', (chunk) => {
+    data += chunk;
+  });
+  writableStream.on('finish', () => {
+    const dataArray = data.split('\n');
+    const splicedData = dataArray.splice(dataArray.length - stringCount, stringCount);
+    const stringOfData = splicedData.join('\n');
+    tailStream.write(stringOfData);
+    tailStream.end();
+    console.log(stringOfData);
+  });
 }
-tail('data.js',9,'data.txt');
+
+tail('datas.txt', 'data.txt');
+
+
