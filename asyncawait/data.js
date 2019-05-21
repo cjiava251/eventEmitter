fad470a4b17301be59f810387dc7844d2603ddd6
cjiava251 <zlivkosyava@gmail.com>
2019-05-21 11:49:05 +0300

    formating code

--- asyncawait/data.js
+++ asyncawait/data.js
@@ -3,21 +3,21 @@ const libMustache = require('mustache');
 let template, view;
 
 function readFile(file) {
-  return new Promise((resolve,reject) => {
-    fs.readFile(file,'utf-8',(error,data) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, 'utf-8', (error, data) => {
       if (error) reject(error);
       else resolve(data);
     });
   });
 }
 
-async function writeFile(jsonFile,htmlFile,buildFile) {
-  view=JSON.parse(await readFile(jsonFile));
-  template=await readFile(htmlFile);
-  fs.writeFile(buildFile,libMustache.render(template,view), (error) => {
+async function writeFile(jsonFile, htmlFile, buildFile) {
+  view = JSON.parse(await readFile(jsonFile));
+  template = await readFile(htmlFile);
+  fs.writeFile(buildFile, libMustache.render(template, view), (error) => {
     if (error) throw error;
     else console.log('success');
   });
 }
-writeFile('data,json','template.html','build.html');
+writeFile('data,json', 'template.html', 'build.html');
 
